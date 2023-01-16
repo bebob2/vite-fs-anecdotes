66485c5419f5ece8abc6d99ea793669689de7316
Benjamin Bobkin <benjamin.bobkin@europace.de>
2023-01-16 14:56:17 +0100

    First creation

---
+++ src/app.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(
+    Array.from({ length: anecdotes.length }, () => 0)
+  );
+
+  // let votes = Array(anecdotes.length).fill(0);
+  return (
+    <>
+      <h1>World's best JOKES</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>Votes: {votes[selected]}</div>
+      <br />
+      <br />
+      <button
+        onClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+      >
+        New Joke
+      </button>
+      <button
+        onClick={() =>
+          setVotes(
+            votes.map((vote, index) => (index === selected ? vote + 1 : vote))
+          )
+        }
+      >
+        Vote
+      </button>
+    </>
+  );
+};
+
+export default App;
